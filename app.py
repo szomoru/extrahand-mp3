0d046156795b79243142bde49f37a9ec8693a76d
szomoru <gergely.vig1978@gmail.com>
2021-11-17 10:37:33 +0000

    profile.html and base_profile.html (template) have been created and profile access functionality added. Logout functionality also added but not finalised

--- app.py
+++ app.py
@@ -96,6 +96,26 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
